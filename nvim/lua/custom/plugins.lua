6bd663d61b47f9fe7e6a64df47ace0a260de41e7
NoOPeEKS <arnauapps@gmail.com>
2024-06-09 00:41:12 +0200

    Add more lines of output in virttext cells

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -7,7 +7,7 @@ local plugins = {
     init = function()
       -- this is an example, not a default. Please see the readme for more configuration options
       vim.g.molten_image_provider = "image.nvim"
-      vim.g.molten_output_win_max_height = 12
+      vim.g.molten_output_win_max_height = 50
     end,
   },
   {
