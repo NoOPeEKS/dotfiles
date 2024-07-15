50a8e7207b642fa435e4317174d0acf53e937fde
NoOPeEKS <arnauapps@gmail.com>
2024-07-15 20:57:56 +0200

    Update nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,28 +1,20 @@
-require "core"
-
-local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
-
-if custom_init_path then
-  dofile(custom_init_path)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
-
-require("core.utils").load_mappings()
-require "custom.keybinds"
-
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
--- bootstrap lazy.nvim!
-if not vim.loop.fs_stat(lazypath) then
-  require("core.bootstrap").gen_chadrc_template()
-  require("core.bootstrap").lazy(lazypath)
-end
-
-dofile(vim.g.base46_cache .. "defaults")
 vim.opt.rtp:prepend(lazypath)
--- vim.g.python3_host_prog = vim.fn.exepath 'python3'
+
+require("vim-options")
+require("lazy").setup("plugins")
 vim.g.python3_host_prog=vim.fn.expand("~/.virtualenvs/neovim/bin/python3")
 vim.g.loaded_python3_provider = nil
 vim.cmd('runtime! plugin/rplugin.vim')
 package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua"
 package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua"
-require "plugins"
--- nvim/lua/custom/keybinds.lua
+++
@@ -1,8 +0,0 @@
-vim.keymap.set("v", "<leader>mv", ":<C-u>MoltenEvaluateVisual<CR>gv",
-    { silent = true, desc = "Molten Evaluate Visual" })
-vim.keymap.set("n", "<leader>ml", function()
-  vim.cmd("MoltenEvaluateLine")
-end, {desc = "Molten Evaluate Line"})
-vim.keymap.set("n", "<leader>mo", ":noautocmd MoltenEnterOutput<CR>",
-    { silent = true, desc = "Molten Enter Output Window" })
-vim.keymap.set("n", "<leader>jc", "i```python<CR><CR>```", {desc = "Jupyter Create Cell"})
